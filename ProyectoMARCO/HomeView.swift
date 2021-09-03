4fe0c6fae240c9190936c65f8603bb43684e100e
Alex Torres <dalextor@gmail.com>
2021-09-03 14:52:53 -0500

    First commit

---
+++ ProyectoMARCO/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  ProyectoMARCO
+//
+//  Created by Alejandro Torres on 03/09/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    enum Tab {
+        case home
+        case other
+    }
+    
+    @State private var tab : Tab = .home
+    
+    var body: some View {
+        TabView(selection: $tab) {
+            
+            NavigationView {
+                Text("Home")
+            }
+            .tabItem {
+                let menuText = Text("Home", comment: "Home")
+                Label {
+                    menuText
+                } icon: {
+                    Image(systemName: "house")
+                }
+            }
+            .tag(Tab.home)
+            
+            NavigationView {
+                Text("Other")
+            }
+            .tabItem {
+                let menuText = Text("Other", comment: "Other")
+                Label {
+                    menuText
+                } icon: {
+                    Image(systemName: "house")
+                }
+            }
+            .tag(Tab.other)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- ProyectoMARCO/ProyectoMARCOApp.swift
+++ ProyectoMARCO/ProyectoMARCOApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ProyectoMARCOApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
