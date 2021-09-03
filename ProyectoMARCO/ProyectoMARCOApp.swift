10a198068583c78771dcd90bcfcd18dd359ea80d
Alex Torres <dalextor@gmail.com>
2021-09-03 14:05:24 -0500

    Initial Commit

---
+++ ProyectoMARCO/ProyectoMARCOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoMARCOApp.swift
+//  ProyectoMARCO
+//
+//  Created by Alejandro Torres on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoMARCOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
