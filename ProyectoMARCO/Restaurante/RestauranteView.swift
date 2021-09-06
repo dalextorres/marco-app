4d53538a3eb83e12518c41b2694863f5c307902a
Alex Torres <dalextor@gmail.com>
2021-09-06 12:18:18 -0500

    Fix file

---
+++ ProyectoMARCO/Restaurante/RestauranteView.swift
@@ -0,0 +1,20 @@
+//
+//  RestauranteView.swift
+//  ProyectoMARCO
+//
+//  Created by Alejandro Torres on 06/09/21.
+//
+
+import SwiftUI
+
+struct RestauranteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestauranteView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestauranteView()
+    }
+}
