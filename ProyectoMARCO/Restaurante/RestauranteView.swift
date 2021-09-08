d65c52332e812421cd11307abc440af815e3c715
Alex Torres <dalextor@gmail.com>
2021-09-08 13:03:21 -0500

    Add WebView

--- ProyectoMARCO/Restaurante/RestauranteView.swift
+++ ProyectoMARCO/Restaurante/RestauranteView.swift
@@ -29,22 +29,22 @@ struct RestauranteView: View {
                 }
             })
             
-            ZStack {
-                Image("MenuMarco")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 280)
-                    .padding()
-                    .onTapGesture {
-                        UIApplication.shared.open(URL(string: "https://www.marco.org.mx/wp-content/uploads/2021/07/menu-restaurante1.pdf")!)
-                }
-                
-                Text("REVISA EL MENU")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("Blanco"))
-                    .multilineTextAlignment(.center)
-            }
+            NavigationLink(destination: WebView(html: "https://www.marco.org.mx/wp-content/uploads/2021/07/menu-restaurante1.pdf"),
+                label: {
+                    ZStack {
+                        Image("MenuMarco")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 280)
+                            .padding()
+                        
+                        Text("REVISA EL MENU")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("Blanco"))
+                            .multilineTextAlignment(.center)
+                    }
+                })
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar(content: {
--- ProyectoMARCO/WebView.swift
+++ ProyectoMARCO/WebView.swift
@@ -12,11 +12,11 @@ public struct WebView: UIViewRepresentable {
 
     let html: String
 
-    func makeUIView(context: Context) -> WKWebView {
+    public func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
     }
 
-    func updateUIView(_ uiView: WKWebView, context: Context) {
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
         uiView.load(URLRequest(url: URL(string: html)!))
         uiView.allowsBackForwardNavigationGestures = false
     }
