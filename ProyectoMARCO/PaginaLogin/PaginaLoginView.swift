8a86cae9e404ccbc1ed2adc8d8f0a7caf95f7c8c
thehaiwave <manueld0293@gmail.com>
2021-10-21 00:27:21 -0700

    Login

--- ProyectoMARCO/PaginaLogin/PaginaLoginView.swift
+++ ProyectoMARCO/PaginaLogin/PaginaLoginView.swift
@@ -17,60 +17,84 @@ struct PaginaLoginView: View {
     var body: some View {
         NavigationView {
             VStack{
-                Text("BIENVENIDO")
+                ZStack {
+                    
+                
+                    
+                    Rectangle()
+                        .fill(Color("Rosa"))
+                        .frame(width: .infinity, height: 300	)
+                        
+                        
+                Image("LogoMarco")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                }
+                .padding(.bottom)
+                    
+                
+                Text("**Bienvenido a Marco**")
                     .multilineTextAlignment(.center)
-                    .font(.custom("Roboto Mono", size: 25))
+                    .font(.system(size: 30))
                     .padding(.bottom, 30)
         
+                VStack {
                 TextField("Correo electronico", text: $loginVM.email)
-                    .padding(4)
+                    .padding(15)
                     .background(lightGreyColor)
                     .cornerRadius(5.0)
                     .font(.custom("Roboto Mono", size: 17))
-                    .frame(height: 50)
+                    .frame(height: 70)
                 SecureField("Contrasena", text: $loginVM.password)
-                    .padding(4)
+                    .padding(15)
                     .background(lightGreyColor)
                     .cornerRadius(5.0)
                     .font(.custom("Roboto Mono", size: 17))
-                    .frame(height: 50)
-                
+                    .frame(height: 70)
+                }
+                .padding(.horizontal, 50)
+                .padding(.bottom, 20)
                 Button(action: {
                         loginVM.login()
                 }, label: {
-                    Text("LOGIN")
+                    Text("**Iniciar Sesion**")
                         .foregroundColor(.white)
                         .padding(.horizontal, 20)
-                        .padding(.vertical, 5)
+                        .padding(.vertical, 10)
                         .font(.custom("Roboto Mono", size: 19))
                     
                 })
                 .background(Color("Rosa"))
                 .cornerRadius(5)
                 
-            
-                NavigationLink(destination: PaginaSignupView()) {
-                    Text("SIGNUP")
+                Spacer()
+        
+                HStack {
+                    Text("No tienes cuenta?")
+                        .font(.custom("Roboto Mono", size: 19))
+                        .padding()
+                    NavigationLink(destination: PaginaSignupView()) {
+                        Text("**Registrate**")
+                            .font(.custom("Roboto Mono", size: 19))
+                           
+                    }
+                    .padding()
                 }
+                Spacer()
             }
-            
+            .edgesIgnoringSafeArea(.top)
+        
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar(content: {
-            ToolbarItem(placement: .principal, content: {
-                HStack{
-                    Text("Museo").foregroundColor(.white)
-                }
-            })
-        })
-    }
+       
+
+        }
 }
 
 struct PaginaLoginView_Previews: PreviewProvider {
     static var previews: some View {
         PaginaLoginView()
+            .environmentObject(LoginViewModel())
     }
 }
 
